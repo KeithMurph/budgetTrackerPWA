7832aa95124adef668e4de4abecf77ea35d53b3f
KeithMurph <keithmurphy94@gmail.com>
2021-08-22 16:03:14 -0700

    dynamic port

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,25 +12,23 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png",
 ];
 
-// install
+
 self.addEventListener("install", function (evt) {
-  // pre cache image data
+
   evt.waitUntil(
     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
   );
 
-  // pre cache all static assets
+
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
+ 
   console.log('service worker running');
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -48,7 +46,7 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
+
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
@@ -57,7 +55,7 @@ self.addEventListener("fetch", function (evt) {
         .then((cache) => {
           return fetch(evt.request)
             .then((response) => {
-              // If the response was good, clone it and store it in the cache.
+              
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -65,7 +63,7 @@ self.addEventListener("fetch", function (evt) {
               return response;
             })
             .catch((err) => {
-              // Network request failed, try to get it from the cache.
+            
               return cache.match(evt.request);
             });
         })
