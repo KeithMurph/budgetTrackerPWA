9f0f031e23714c7360948fc7cf1a0c432a9a1b92
KeithMurph <keithmurphy94@gmail.com>
2021-08-22 16:40:14 -0700

    wtftffft

--- public/service-worker.js
+++ public/service-worker.js
@@ -30,7 +30,7 @@ self.addEventListener("activate", function(evt) {
       return Promise.all(
         keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
+            console.log("cache cleared", key);
             return caches.delete(key);
           }
         })
