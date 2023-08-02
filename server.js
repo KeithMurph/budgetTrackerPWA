dbd4e5be51138cc508b93a3b7a0eb98d78369bb9
KeithMurph <keithmurphy94@gmail.com>
2023-08-01 20:53:58 -0700

    idk its not working

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useFindAndModify: false,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 
